ed14ae24965c4f16b5521ef501b297bcb2ba706a
Sarah Rawlinson <srrawlinson@hotmail.co.uk>
2022-11-01 21:18:45 +0000

    other tests working

--- tests/PlayerCreatorTest.php
+++ tests/PlayerCreatorTest.php
@@ -1,16 +1,28 @@
 ﻿<?php
 //use Poker\Classes\Person\PlayerCreator;
 //use Poker\Classes\Person\PlayerType;
+//https://pestphp.com/docs/expectations#expect-toBeCallable
 
 include_once ("Include.php");
 
+$objects = 
+    [["bob", PlayerType::AI],
+    ["bob", PlayerType::Human]];
 
 
-it("check creation", function (string $name, PlayerType $type)
+test("check creation", function ($name, $type)
 {
     $this->assertSame(PlayerCreator::CreatePlayer($type, $name)->getName(), $name);
-})->with([
-    ["bob", PlayerType::AI],
-    ["bob", PlayerType::Human]
-]);
+})->with($objects);
 
+
+test("check players instance of IPerson", function ($name, $type)
+{
+    expect(PlayerCreator::CreatePlayer($type, $name))->toBeInstanceOf(IPerson::class);
+})->with($objects);
+
+
+test("check name is string", function ($name, $type)
+{
+    expect(PlayerCreator::CreatePlayer($type, $name)->getName())->toBeString();
+})->with($objects);
