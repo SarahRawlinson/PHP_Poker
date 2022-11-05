8721d868936609664d624d8a1e34ca61a6ed4fda
Sarah Rawlinson <srrawlinson@hotmail.co.uk>
2022-11-05 02:42:29 +0000

    Update Include.php

--- Include.php
+++ Include.php
@@ -1,7 +1,16 @@
 ﻿<?php
 
-include ("Junk\Number.php");
 include("Classes\Player\IPerson.php");
 include("Classes\Player\PlayerCreator.php");
 include("Classes\Player\AI.php");
 include("Classes\Player\Human.php");
+include ("Classes\Player\PlayerGroup.php");
+include ("Classes\Cards\Card\ICard.php");
+include ("Classes\Cards\Card\Rank.php");
+include ("Classes\Cards\Card\StandardPlayingCard.php");
+include ("Classes\Cards\Card\Suit.php");
+include ("Classes\Cards\Dealer\IDealer.php");
+include ("Classes\Cards\Dealer\StandardPlayerCardDealer.php");
+include ("Classes\Cards\Hand\IHand.php");
+include ("Classes\Cards\Hand\StandardPlayerCardHand.php");
+include ("Classes\Cards\Deck.php");
