c7551c29f10d928a86157cd9c13eac6705e8f9dc
Sarah Rawlinson <sarahrosannerawlinson@gmail.com>
2022-11-20 17:10:39 +0000

    post factory and more tests

--- database/migrations/2022_11_20_125302_add_user_id_to_posts_table.php
+++ database/migrations/2022_11_20_125302_add_user_id_to_posts_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
             else
             {
                 $table->foreignId('user_id')
-                    ->nullable()
                     ->constrained()
                     ->onDelete('cascade');
             }
--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class DatabaseTest extends TestCase
@@ -12,11 +14,40 @@ class DatabaseTest extends TestCase
 
     public function test_the_post_model()
     {
-        Post::create([
-            'title' => 'Hello',
-            'description' => 'Hello there',
-        ]);
+        Post::factory()->create();
 
         $this->assertDatabaseCount('posts', 1);
+    }
+
+    public function test_the_post_model_6()
+    {
+        Post::factory()->count(6)->create();
+
+        $this->assertDatabaseCount('posts', 6);
+    }
+
+    public function test_the_post_user_relationship()
+    {
+        $user = User::factory()
+            ->has(Post::factory()->count(5))
+//            ->hasPosts(5)
+            ->create();
+        $this->assertDatabaseCount('posts',5);
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('posts', ['user_id' => $user->id]);
+    }
+
+    public function test_the_authentication()
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('abc123'),
+        ]);
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'abc123'
+        ]);
+
+        $this->assertAuthenticated();
     }
 }
