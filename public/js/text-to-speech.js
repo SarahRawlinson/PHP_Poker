d3bb6a102061baad75234a37444b50737f22f784
Sarah Rawlinson <sarahrosannerawlinson@gmail.com>
2023-02-08 21:11:15 +0000

    added text to speech to website

---
+++ public/js/text-to-speech.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+    const synth = window.speechSynthesis;
+    const text_to_speech_div = $('#text-to-speech');
+    const speak = (text) => {
+        const utterance = new SpeechSynthesisUtterance(text);
+        synth.speak(utterance);
+    }
+
+    const createInput = () => {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.id = "text-input";
+        text_to_speech_div.append(input);
+    }
+
+    const createButton = () => {
+        const button = document.createElement("button");
+        button.innerHTML = "Speak";
+        button.id = "speak-button";
+        button.addEventListener("click", () => {
+            const textInput = document.querySelector("#text-input");
+            speak(textInput.value);
+        });
+        text_to_speech_div.append(button);
+    }
+
+    createInput();
+    createButton();
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,11 +20,15 @@
             <small class="row">time: <b>{{$post->updated_at}}</b></small>
         </div>
 
-        <div class="game-viewer container d-flex justify-content-center">
-            @include('unity.chess-game-v1')
-        </div>
+{{--        <div class="game-viewer container d-flex justify-content-center">--}}
+{{--            @include('unity.chess-game-v1')--}}
+{{--        </div>--}}
     @empty
         <p>There are no posts yet</p>
     @endforelse
+    <div id="text-to-speech">
+
+    </div>
 @endsection
+<script src="{{asset('js/text-to-speech.js')}}"></script>
 
